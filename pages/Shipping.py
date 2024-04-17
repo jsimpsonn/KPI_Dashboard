10533218074d9a8c2174f37a6a7884a3d5ea15f2
jsimpsonn <jsimpson@mssteelpro.com>
2024-04-17 18:01:19 -0500

    added dataframes below charts

--- pages/Shipping.py
+++ pages/Shipping.py
@@ -8,6 +8,9 @@ st.set_page_config(
     page_icon="assets/MSP_Favicon.png",
 )
 
+st.title("Shipping")
+st.subheader("Summary")
+
 # Read the shipping data
 df_shipping = pd.read_csv("data/shipping.csv")
 
@@ -18,8 +21,27 @@ df_shipping['Date'] = pd.to_datetime(df_shipping['Date'], format='%m/%d/%Y')
 df_shipping_sorted = df_shipping.sort_values(by='Date')
 
 # Create the plot
-shipping = px.line(df_shipping_sorted, x="Date", y="Percent", title="Shipping - % of Trucks Loaded in One Hour or Less")
+shipping = px.line(df_shipping_sorted, x="Date", y="Percent")
 
 # Display the plot
-st.header("Shipping")
 st.plotly_chart(shipping)
+
+st.subheader("Data")
+
+tabAll, tab2021, tab2022, tab2023, tab2024 =st.tabs(["All", "2021", "2022", "2023", "2024"])
+
+df_2021 = df_shipping[df_shipping["Date"].dt.year == 2021]
+df_2022 = df_shipping[df_shipping["Date"].dt.year == 2022]
+df_2023 = df_shipping[df_shipping["Date"].dt.year == 2023]
+df_2024 = df_shipping[df_shipping["Date"].dt.year == 2024]
+
+with tabAll:
+    st.dataframe(df_shipping,use_container_width=True)
+with tab2021:
+    st.dataframe(df_2021,use_container_width=True)
+with tab2022:
+    st.dataframe(df_2022,use_container_width=True)
+with tab2023:
+    st.dataframe(df_2023,use_container_width=True)
+with tab2024:
+    st.dataframe(df_2024,use_container_width=True)
