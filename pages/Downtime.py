3558b4d79188ca38be590d57ac98fd1763768081
John Simpson <jsimpson@mssteelpro.com>
2024-05-06 14:06:15 -0500

    "Caching for performance"

--- pages/Downtime.py
+++ pages/Downtime.py
@@ -4,12 +4,12 @@ import plotly.express as px
 
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 
+@st.cache_data
 def load_data():
     # Load maintenance and non-maintenance data from three different lines
     data_frames = []
     lines = ['stamco', 'redbud', 'braner']
     types = ['mr', 'nmr']
-
     for line in lines:
         for dtype in types:
             temp_df = pd.read_csv(f'data/downtime/{line}/{line}_{dtype}.csv')
@@ -21,14 +21,12 @@ def load_data():
             temp_df['Line'] = line.capitalize()
             temp_df['Maintenance Status'] = 'Maintenance Related' if dtype == 'mr' else 'Non-Maintenance Related'
             data_frames.append(temp_df)
-
     # Combine all data into a single DataFrame
     combined_data = pd.concat(data_frames, ignore_index=True)
     return combined_data
 
 def main():
     st.header("Downtime Analysis by Production Line")
-    
     combined_data = load_data()
 
     # User selection for individual line or all lines
@@ -46,16 +44,19 @@ def main():
 
     for month in selected_months:
         # Prepare data for the selected month
-        month_data = combined_data.melt(id_vars=['Category', 'Line', 'Maintenance Status'], value_vars=[month], 
+        month_data = combined_data.melt(id_vars=['Category', 'Line', 'Maintenance Status'], value_vars=[month],
                                         var_name='Month', value_name='Downtime')
         month_data = month_data[month_data['Downtime'] != 0]  # Filter out zero downtime entries
 
         # Generate the treemap
-        fig_treemap = px.treemap(month_data, path=['Line', 'Maintenance Status', 'Category'] if selected_line == 'All Lines' else ['Maintenance Status', 'Category'], 
-                                 values='Downtime', title=f"{month} Downtime - {selected_line}", color='Downtime', color_continuous_scale=color_scale)
-        fig_treemap.update_layout(margin=dict(t=50, l=25, r=25, b=25),
-                                  plot_bgcolor='rgba(0,0,0,0)')
+        fig_treemap = px.treemap(month_data, path=['Line', 'Maintenance Status', 'Category'] if selected_line == 'All Lines' else ['Maintenance Status', 'Category'],
+                                 values='Downtime', title=f"{month} Downtime - {selected_line}",
+                                 color='Downtime', color_continuous_scale=color_scale)
+        fig_treemap.update_layout(margin=dict(t=50, l=25, r=25, b=25), plot_bgcolor='rgba(0,0,0,0)')
         fig_treemap.update_traces(marker=dict(cornerradius=5))
-        st.plotly_chart(fig_treemap, use_container_width=True)
+
+        # Display the treemap using fragmentation
+        with st.container():
+            st.plotly_chart(fig_treemap, use_container_width=True)
 
 main()
